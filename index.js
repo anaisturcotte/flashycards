ac7100da05f7cafefa19f8008a2ed971eca83bad
Stan Robotix <nikola.ellis@gmail.com>
2024-04-28 21:38:56 -0400

    Login form

--- index.js
+++ index.js
@@ -1,55 +1,83 @@
 // Imports
+const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
-const app = express();
-const mysql = require('mysql');
+const path = require('path');
+
+// Connections
 const port = 5100;
+
+//-------------------Lien avec sql-------------------
+const connection = mysql.createConnection({
+	host     : '127.0.0.1',
+	user     : 'nikola',
+	password : '123456789',
+	database : 'nodelogin'
+});
+
+const app = express();
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database as ID ' + connection.threadId);
+});
 
 // accès au site sur host local:
 // http://localhost:5100/index.html#
 
-app.use(express.static('public'));
-
-//-------------------Initialiser le serveur-------------------
-app.listen(port, () => console.info(`App listening on port ${port}`))
-
 app.use(session({
-	secret: 'poop',
+	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//---------------------lien avec html---------------------
+
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //---------------------Login----------------------
 app.post('/auth', function(request, response) { 
-	// Capture the input fields
-	console.log(`request.body=${JSON.stringify(request.body)}`)
-	let username = request.body.username;
-	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
-				response.redirect('/profil');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username:' + username + 'Password:' + password);
-		response.end();
-	}
+    // Capture the input fields
+    console.log(`request.body=${JSON.stringify(request.body)}`)
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exist and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) {
+                console.error('Error executing SQL query: ' + error);
+                response.send('An error occurred. Please try again later.');
+            } else {
+                // If the account exists
+                if (results.length > 0) {
+                    // Authenticate the user
+                    request.session.loggedin = true;
+                    request.session.username = username;
+                    // Redirect to home page
+                    response.redirect('/profil');
+                } else {
+                    response.send('Incorrect Username and/or Password!');
+                }
+            }
+            response.end();
+        });
+    } else {
+        response.send('Username or password missing.');
+        response.end();
+    }
 });
+
 
 app.get('/profil', function(request, response) {
 	// If the user is loggedin
@@ -62,3 +90,7 @@ app.get('/profil', function(request, response) {
 	}
 	response.end();
 });
+
+
+//-------------------Initialiser le serveur-------------------
+app.listen(port, () => console.info(`App listening on port ${port}`))
