d99f06f069c5d47c48ca5aff781bbc0f0a92952c
Stan Robotix <nikola.ellis@gmail.com>
2024-05-03 13:37:59 -0400

    template home

--- server.js
+++ server.js
@@ -58,128 +58,132 @@ app.get("/index", (req, res) => {
 });
 
 // app.get('/', function(request, response) {
-    // 	// Render login template
-    // 	response.sendFile(path.join(__dirname, 'views/index.html'));
-    //     // response.sendFile(path.join(__dirname, 'views/explore.html'));
-    //     response.end()
-    // });
+// 	// Render login template
+// 	response.sendFile(path.join(__dirname, 'views/index.html'));
+//     // response.sendFile(path.join(__dirname, 'views/explore.html'));
+//     response.end()
+// });
     
     
-    //---------------------Login----------------------
-    app.post('/login', function(request, response) { 
-        // Capture the input fields
-        console.log(`request.body=${JSON.stringify(request.body)}`)
-        let username = request.body.username;
-        let password = request.body.password;
-        // Ensure the input fields exist and are not empty
-        if (username && password) {
-            // Execute SQL query that'll select the account from the database based on the specified username and password
-            connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-                // If there is an issue with the query, output the error
-                if (error) {
-                    console.error('Error executing SQL query: ' + error);
-                    response.send('An error occurred. Please try again later.');
-                } else {
-                    // If the account exists
-                    if (results.length > 0) {
-                        // Authenticate the user
-                        request.session.loggedin = true;
-                        request.session.username = username;
-                        // Redirect to home page
-                        console.log(`redirecting... utilisateur=${request.session.username}`);
-                        response.redirect(`/home?utilisateur=${request.session.username}`);
-                    } else {
-                        response.send('Incorrect Username and/or Password!');
-                    }
-                }
-                response.end();
-            });
-        } else {
-            response.send('Username or password missing.');
-            response.end();
-        }
-    });
-    
-    
-    //-------------------Sign Up-------------------
-    app.post('/signup', function(request, response) { 
-        // Capture the input fields
-        console.log(`request.body=${JSON.stringify(request.body)}`)
-        let newUsername = request.body.newUsername;
-        let newEmail = request.body.newEmail;
-        let newPassword = request.body.newPassword;
-        // Ensure the input fields exist and are not empty
-        if (newUsername && newPassword) {
-            // Execute SQL query that'll select the account from the database based on the specified newUsername and newPassword
-            connection.query('INSERT INTO accounts (username, email, password) VALUES (?, ?, ?)', [newUsername, newEmail, newPassword], function(error, results, fields) {
-                // If there is an issue with the query, output the error
-                if (error) {
-                    console.error('Error executing SQL query: ' + error);
-                    response.send('An error occurred. Please try again later.');
-                } else {
+//---------------------Login----------------------
+app.post('/login', function(request, response) { 
+    // Capture the input fields
+    console.log(`request.body=${JSON.stringify(request.body)}`)
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exist and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) {
+                console.error('Error executing SQL query: ' + error);
+                response.send('An error occurred. Please try again later.');
+            } else {
+                // If the account exists
+                if (results.length > 0) {
                     // Authenticate the user
                     request.session.loggedin = true;
-                    request.session.username = newUsername;
+                    request.session.username = username;
                     // Redirect to home page
-                    console.log(`redirecting... utilisateur=${request.session.username}`);
-                    response.redirect(`/home.html?utilisateur=${request.session.username}`);
+                    console.log(`redirecting... username=${request.session.username}`);
+                    response.redirect(`/home?username=${request.session.username}`);
+                } else {
+                    response.send('Incorrect Username and/or Password!');
                 }
-                response.end();
-            });
-        } else {
-            response.send('Username or password missing.');
+            }
             response.end();
+        });
+    } else {
+        response.send('Username or password missing.');
+        response.end();
+    }
+});
+
+
+//-------------------Sign Up-------------------
+app.post('/signup', function(request, response) { 
+    // Capture the input fields
+    console.log(`request.body=${JSON.stringify(request.body)}`)
+    let newUsername = request.body.newUsername;
+    let newEmail = request.body.newEmail;
+    let newPassword = request.body.newPassword;
+    // Ensure the input fields exist and are not empty
+    if (newUsername && newPassword) {
+        // Execute SQL query that'll select the account from the database based on the specified newUsername and newPassword
+        connection.query('INSERT INTO accounts (username, email, password) VALUES (?, ?, ?)', [newUsername, newEmail, newPassword], function(error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) {
+                console.error('Error executing SQL query: ' + error);
+                response.send('An error occurred. Please try again later.');
+            } else {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.username = newUsername;
+                // Redirect to home page
+                console.log(`redirecting... username=${request.session.username}`);
+                response.redirect(`/home.html?username=${request.session.username}`);
+            }
+            response.end();
+        });
+    } else {
+        response.send('Username or password missing.');
+        response.end();
+    }
+});
+
+app.get("/home", (req, res) => {
+    const { username } = req.query;
+    res.render("home", { username });
+});
+    
+    
+// ----------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------- Openning a card set ------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+
+const _ = require('lodash');
+
+let card_set = {
+    ids:[],
+    terms: [],
+    definitions: [],
+    hints: [],
+    knownLevel: [],
+    randNumList: []
+}
+
+// example of results :
+// [
+    //     { id: 1, name: 'Card 1', description: 'Description of Card 1', idEnsemble: 1 },
+    //     { id: 2, name: 'Card 2', description: 'Description of Card 2', idEnsemble: 1 },
+    //     { id: 3, name: 'Card 3', description: 'Description of Card 3', idEnsemble: 2 }
+    // ]
+        
+function open(cardSet) {
+    console.log(`open(cardSet) entered !!!`);
+    connection.query("select * from cartes where ensemble.nomEnsemble=? and ensemble.id=cartes.idEnsemble", [cardSet], function(error, results, fields) {
+        if (error) {
+            console.error('Error executing SQL query: ' + error);
+        } else {
+            var randomNumber = 0;
+            for (let i = 0; i <= results.length; i++) {
+                card_set.ids.push(results[i]['id']);
+                card_set.terms.push(results[i]['motTerme']);
+                card_set.definitions.push(results[i]['motDefinition']);
+                card_set.hints.push(results[i]['aide']);
+                card_set.knownLevel.push(results[i]['nivConnu']);
+                card_set.randNumList.push(randomNumber = _.random(1, 10));
+            }
+            console.log(`card_set: ${card_set}`);
         }
     });
-    
-    
-    
-    // ----------------------------------------------------------------------------------------------------------- //
-    // ------------------------------------------- Openning a card set ------------------------------------------- //
-    // ----------------------------------------------------------------------------------------------------------- //
-    
-    const _ = require('lodash');
-    
-    let card_set = {
-        ids:[],
-        terms: [],
-        definitions: [],
-        hints: [],
-        knownLevel: [],
-        randNumList: []
-    }
-    
-    // example of results :
-    // [
-        //     { id: 1, name: 'Card 1', description: 'Description of Card 1', idEnsemble: 1 },
-        //     { id: 2, name: 'Card 2', description: 'Description of Card 2', idEnsemble: 1 },
-        //     { id: 3, name: 'Card 3', description: 'Description of Card 3', idEnsemble: 2 }
-        // ]
+}
         
-        function open(cardSet) {
-            console.log(`open(cardSet) entered !!!`);
-            connection.query("select * from cartes where ensemble.nomEnsemble=? and ensemble.id=cartes.idEnsemble", [cardSet], function(error, results, fields) {
-                if (error) {
-                    console.error('Error executing SQL query: ' + error);
-                } else {
-                    var randomNumber = 0;
-                    for (let i = 0; i <= results.length; i++) {
-                        card_set.ids.push(results[i]['id']);
-                        card_set.terms.push(results[i]['motTerme']);
-                        card_set.definitions.push(results[i]['motDefinition']);
-                        card_set.hints.push(results[i]['aide']);
-                        card_set.knownLevel.push(results[i]['nivConnu']);
-                        card_set.randNumList.push(randomNumber = _.random(1, 10));
-                    }
-                    console.log(`card_set: ${card_set}`);
-                }
-            });
-        }
-        
-        // ------------------------------------------------------------------------------------------------------------- //
-        // ----------------------------------------------- with MUSTACHE ----------------------------------------------- //
-        // ------------------------------------------------------------------------------------------------------------- //
-        
-        
-        //-------------------Initialiser le serveur-------------------
-        app.listen(port, () => console.info(`App listening on port ${port}`))
+// ------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------- with MUSTACHE ----------------------------------------------- //
+// ------------------------------------------------------------------------------------------------------------- //
+
+
+//-------------------Initialiser le serveur-------------------
+app.listen(port, () => console.info(`App listening on port ${port}`))
